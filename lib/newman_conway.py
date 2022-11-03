706ade302a30cbbdf2ef6020aa801245180c363d
Shayla Logan <shayla.logan709@gmail.com>
2022-11-03 09:43:14 -0700

    Initial commit

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -3,4 +3,18 @@ def newman_conway(num):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if num <= 0:
+        raise ValueError(0)
+    else: 
+        arr = [0, 1, 1]
+
+    if num == 1 or num == 2:
+        return '1'
+    else:
+        for i in range(3, num + 1):                  
+            p = arr[arr[i - 1]] + arr[i - arr[i - 1]]
+            arr.append(p)
+    data = ' '.join(str(n) for n in arr[1:])
+    return data
+
+print(newman_conway(4))
