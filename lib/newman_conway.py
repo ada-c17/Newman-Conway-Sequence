6a0608aacf083cecc781300ef1c8f2ce47d16cf6
Georgia Adam <georgiaeadam@gmail.com>
2022-11-06 20:14:52 -0500

    tests passing

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,6 +1,26 @@
+import array
+
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+    if num <= 0:
+        raise ValueError
+    elif num == 1:
+        return '1'
+
+    f = array.array('i', [0, 1, 1])
+
+    for i in range(3, num + 1):
+        r = f[f[i - 1]] + f[i - f[i - 1]]
+        f.append(r)
+
+    res = ''
+    for i in range(1, len(f)):
+        if i == len(f) - 1:
+            res += str(f[i])
+            break
+        res += f'{f[i]} '
+
+    return res
