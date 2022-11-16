c0522d85b03426bcf64bc5d629eb4f714d57f4d3
shelby-faulconer <shelby.faulconer@disneystreaming.com>
2022-11-15 16:08:16 -0800

    wrote pseudo-code and function

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -3,4 +3,19 @@ def newman_conway(num):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+
+    cache = [0,1,1]
+    if num == 0:
+        raise valueError
+    if num == 1:
+        return 1
+    if num == 2:
+        return " ".join(cache[1:])
+    if num >= 3:
+    # for loop that iterates through num
+        for n in range(3,num):
+    # P(P(n - 1)) + P(n - P(n - 1))
+            cache.append(cache[cache[n-1]] + cache[n - cache[n - 1]])
+    # append this ^
+        return " ".join(cache[1:])
+
