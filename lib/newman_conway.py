22565d7721468099b3ee80243ecde36b715ce140
shelby-faulconer <shelby.faulconer@disneystreaming.com>
2022-11-15 19:14:39 -0800

    prettier and big o

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,7 +1,7 @@
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: o(n)
+        Space Complexity: o(n)
     """
 
     cache = [0,1,1]
@@ -13,11 +13,8 @@ def newman_conway(num):
         cache_string = [str(x) for x in cache]
         return " ".join(cache_string[1:])
     if num >= 3:
-    # for loop that iterates through num
         for n in range(3,num+1):
-    # P(P(n - 1)) + P(n - P(n - 1))
             cache.append(cache[cache[n-1]] + cache[n - cache[n - 1]])
-    # append this ^
     cache_string = [str(x) for x in cache]
     return " ".join(cache_string[1:])
 
