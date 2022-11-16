26d497e8ebfde3d1d227c57dadb32e6149fca6a6
shelby-faulconer <shelby.faulconer@disneystreaming.com>
2022-11-15 16:33:26 -0800

    yay all tests pass

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -5,17 +5,19 @@ def newman_conway(num):
     """
 
     cache = [0,1,1]
-    if num == 0:
-        raise valueError
+    if num <= 0:
+        raise ValueError(ValueError)
     if num == 1:
-        return 1
+        return '1'
     if num == 2:
-        return " ".join(cache[1:])
+        cache_string = [str(x) for x in cache]
+        return " ".join(cache_string[1:])
     if num >= 3:
     # for loop that iterates through num
-        for n in range(3,num):
+        for n in range(3,num+1):
     # P(P(n - 1)) + P(n - P(n - 1))
             cache.append(cache[cache[n-1]] + cache[n - cache[n - 1]])
     # append this ^
-        return " ".join(cache[1:])
+    cache_string = [str(x) for x in cache]
+    return " ".join(cache_string[1:])
 
