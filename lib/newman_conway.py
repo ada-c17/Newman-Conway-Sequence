a6f7da56dce70be6ddb5811c6f79c0f632e4f0a9
Hillary Smith <hillaryhsmith@gmail.com>
2022-11-24 07:50:54 -0800

    Create Newman Conway sequence function

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,6 +1,26 @@
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Where n is the number of integers in the output string,
+        Time Complexity: O(num)
+        Space Complexity: O(num)
     """
-    pass
+
+    if num <= 0:
+        raise ValueError
+    
+    if num == 1:
+        return '1'
+
+    return ' '.join(str(n) for n in sequence(num))
+
+def sequence(n):
+    result = [None] * n
+    result[0] = 1
+    result[1] = 1
+
+    for i in range(2, n):
+        result[i] = result[result[i - 1] - 1] + result[i - result[i - 1]]
+    
+    return result
+
+
