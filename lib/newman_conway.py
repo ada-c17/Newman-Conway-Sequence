d0aaeb043c8891cf39e317d11d5375b82d44abff
Doina Colun <doinacolun@macpro-2.local>
2022-11-27 21:35:48 -0800

    Implement Newman-Conway Sequence

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -3,4 +3,28 @@ def newman_conway(num):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if num <= 0:
+        raise ValueError
+
+    cache = {}
+
+    result = []
+    for n in range(1, num + 1):
+        result.append(str(getNum(n, cache)))
+
+    return ' '.join(result)
+
+
+def getNum(n, cache):
+    if n in cache:
+        return cache[n]
+
+    if n == 1:
+        cache[1] = 1
+        return 1
+    if n == 2:
+        cache[2] = 1
+        return 1
+    result = getNum(getNum(n - 1, cache), cache) + getNum(n - getNum(n - 1, cache), cache)
+    cache[n] = result
+    return result
