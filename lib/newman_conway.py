4f764d9c6b6d20bbbaab453b06c7804361913931
olive <olivealavine@gmail.com>
2022-11-27 20:15:50 -0600

    iterative solution

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,6 +1,24 @@
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(N)
+        Space Complexity: O(N)
     """
-    pass
+    if num <= 0:
+        raise(ValueError)
+    if num == 1:
+        return '1'
+    
+    solutions = [0,1,1]
+    current = 3
+
+    while current <= num:
+        solutions.append(solutions[solutions[current-1]] + solutions[current - solutions[current - 1]])
+        current += 1
+
+    return ' '.join(str(x) for x in solutions[1:])
+
+
+# P(1) = 1
+# P(2) = 1
+# for all n > 2
+# P(n) = P(P(n - 1)) + P(n - P(n - 1))
