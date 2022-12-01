127722199a9b0139974030ce11b5163b18de9053
Lindsey M Still <lindseymstill@gmail.com>
2022-12-01 10:11:45 -0500

    made tests pass

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,6 +1,22 @@
+import array
 def newman_conway(num):
-    """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
-    """
-    pass
+    if num <= 0:
+        raise ValueError()
+    elif num == 1:
+        return "1"
+    elif num == 2:
+        return "1 1"
+    result = []  
+    def newman(n, f):
+        f = [0, 1, 1]
+    
+        # To store values of sequence in array
+        for i in range(3, n + 1):
+            r = f[f[i-1]]+f[i-f[i-1]]
+            f.append(r);
+
+        return f
+    result = newman(num, result)[1:]
+    result = [str(i) for i in result]
+    return " ".join(result)
+    
