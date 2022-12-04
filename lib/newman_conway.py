ab9a66dc06807c9d162953afd4931ef738537696
Danielle <dmbecker24@gmail.com>
2022-12-03 17:09:26 -0800

    Attempt 1

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,6 +1,25 @@
+
+import array
 def newman_conway(num):
-    """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
-    """
-    pass
+    val = 0
+
+    if num <= 0:
+        raise ValueError
+    
+    if num == 1:
+        return str(1)
+    
+    if num == 2:
+        return "1 1"
+    
+    arr = array.array("i", [1, 1])
+
+    for i in range(1, num+1):
+        val = arr[arr[i - 1]] + arr[i-arr[i-1]]
+        arr.append(val)
+    
+    result = ' '.join(str(elem) for elem in arr)
+
+    return result
+
+
