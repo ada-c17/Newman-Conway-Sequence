720f97c282ad48f8f700c0c6328c37597e5b299f
Danielle <dmbecker24@gmail.com>
2022-12-03 17:17:46 -0800

    Pass all tests

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -12,12 +12,13 @@ def newman_conway(num):
     if num == 2:
         return "1 1"
     
-    arr = array.array("i", [1, 1])
+    arr = array.array("i", [0, 1, 1])
 
-    for i in range(1, num+1):
+    for i in range(3, num+1):
         val = arr[arr[i - 1]] + arr[i-arr[i-1]]
         arr.append(val)
     
+    arr.pop(0)
     result = ' '.join(str(elem) for elem in arr)
 
     return result
