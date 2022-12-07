49ec10051d98a7e884d81e81dee0d068087f669a
Saria Pastor <saria.pastor@protonmail.com>
2022-12-06 22:42:37 -0500

    first working implementation

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,6 +1,15 @@
-def newman_conway(num):
+def newman_conway(num: int) -> str:
     """ Returns a list of the Newman Conway numbers for the given value.
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if num < 1:
+        raise ValueError('Input must be a positive integer')
+    sequence = []
+    i = 0
+    for i in range(num):
+        sequence.append(str(p(i + 1)))
+    return ' '.join(sequence)
+
+def p(num: int) -> int:
+    return 1 if num < 3 else p(p(num - 1)) + p(num - p(num - 1))
