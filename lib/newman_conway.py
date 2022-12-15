6300e0575b2452a012ddb046b9b379124d88c7f0
Shari <shari.fedak@gmail.com>
2022-12-15 14:07:04 -0500

    Update newman_conway.py

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,6 +1,22 @@
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n) because we iterate through the list once
+        Space Complexity: O(n) because we create a list of size n
     """
-    pass
+    if num <= 0:
+        raise ValueError("Input must be greater than 0")
+    newman_conway_list = [0,1, 1]
+    if num < 3:
+        newman_conway_list = newman_conway_list[:num + 1]
+        return newman_conway_list_to_string(newman_conway_list)
+
+    for i in range(3, num + 1):
+        newman_conway_list.append(newman_conway_list[newman_conway_list[i - 1]] + newman_conway_list
+[i - newman_conway_list[i - 1]])
+    return newman_conway_list_to_string(newman_conway_list)
+        
+def newman_conway_list_to_string(list):
+    result = ""
+    for i in list[1:]:
+        result += str(i) + " "
+    return result[:-1]
