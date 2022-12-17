aa41876a8f03c9cc5861e88b21d64b34ab322adf
Liliana <liliana.m.parra@gmail.com>
2022-12-17 17:01:22 -0500

    Passes all tests

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,6 +1,23 @@
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+    if num < 1:
+        raise ValueError("Input cannot be less than one.")
+    if num == 1:
+        return "1"
+
+    memo = [0, 1, 1]
+
+    count = 3
+    while count <= num:
+        memo.append(memo[memo[count - 1]] + memo[count - memo[count - 1]])
+        count += 1
+
+    string = [str(item) for item in memo]
+
+    answer = " ".join(string[1:])
+
+    return answer
+    
