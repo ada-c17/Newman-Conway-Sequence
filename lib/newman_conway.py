895f24d3afe650f4c668d233b6ba1389e03c52e2
Kelly Kang <shkx281@gmail.com>
2023-01-07 17:37:47 -0800

    Passed all tests

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -3,4 +3,17 @@ def newman_conway(num):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    
+    if num < 1:
+        raise ValueError()
+
+    if num == 1:
+        return str(1)
+
+    nc = [0, 1, 1]
+
+    for i in range(3, num + 1):
+        maths = nc[nc[i - 1]] + nc[i - nc[i - 1]]
+        nc.append(maths)
+
+    return " ".join(str(x) for x in nc[1:])
