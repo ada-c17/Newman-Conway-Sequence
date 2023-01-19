b677c43bf87d8d94902c98bb4796fa19fa31b388
Olena <olenazachkow@gmail.com>
2023-01-18 20:59:37 -0800

    solution

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,6 +1,12 @@
-def newman_conway(num):
-    """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
-    """
-    pass
+def newman_conway(n):
+    if n <= 0:
+        raise ValueError("Input should be greater than 0")
+    elif n == 1:
+        return '1'
+    elif n == 2:
+        return '1 1'
+    else:
+        a = [1, 1, 1]
+        for i in range(3, n + 1):
+            a.append(a[a[i-1]] + a[i-a[i-1]])
+        return ' '.join(str(x) for x in a[1:])
