31c1909ee82386845e248b42c676d91c70dced7e
Taingleap Chheang <chheangtaingleap@gmail.com>
2023-01-18 17:06:34 -0800

    passed all tests

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -3,4 +3,17 @@ def newman_conway(num):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if num <= 0:
+        raise ValueError
+    if num == 1:
+        return "1" 
+    
+    dp = [0] * (num+1)
+    dp[1] = 1
+    dp[2] = 1
+    for x in range(3, num+1):
+        dp[x] = dp[dp[x-1]] + dp[x - dp[x-1]]
+    
+    dp_str = [str(n) for n in dp[1:]]
+    
+    return " ".join(dp_str)
