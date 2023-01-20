77613642d53352157994db61cf6526ea57a65037
Jae <j.kerr.clayton@gmail.com>
2023-01-20 06:25:46 -0500

    tests passing

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -3,4 +3,10 @@ def newman_conway(num):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if num <= 0:
+        raise ValueError("invalid input")
+    seq = [1,1]
+    for i in range(2,num):
+        seq.append(seq[seq[i-1]-1]+seq[i-seq[i-1]])
+    return ' '.join(map(str, seq[:num]))
+
