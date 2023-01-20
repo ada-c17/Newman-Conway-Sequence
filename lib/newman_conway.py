f95c583a9d8d8fb2d276e73e5ff5c1198f876f5c
Huma Hameed <mrskq1@gmail.com>
2023-01-19 20:58:22 -0500

    passed all tests

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -3,4 +3,14 @@ def newman_conway(num):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    values = []
+    if num < 1:
+        raise ValueError("Please enter a positive number.")
+    for i in range(1,num+1):
+        values.append(dp(i, values))
+    return " ".join(map(str, values))
+
+def dp(num, values):
+    if num == 1 or num ==2:
+        return 1
+    return values[values[num-2]-1]+values[num - values[num-2]-1]
