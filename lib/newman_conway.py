5580789bbac307e8662a3a849d41f6c1f76ae155
Kiana <kianashui@gmail.com>
2023-01-19 16:30:57 -0800

    refactored to be O(n) time complexity and use memo instead of global dictionary

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,34 +1,20 @@
-nc_dict = {
-    1: 1,
-    2: 1
-}
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n) where n is the input
+        Space Complexity: O(n) where n is the input
     """
 
-    if num <= 0:
+    if num < 1:
         raise ValueError
     elif num == 1:
-        return str(nc_dict[1])
+        return "1"
+
+    memo = [0, 1, 1]
     
+    output_string = "1 1 "
     for i in range(3, num + 1):
-        nc_dict[i] = newman_conway_helper(nc_dict[i - 1]) + newman_conway_helper(i - nc_dict[i - 1])
+        new_number = memo[memo[i - 1]] + memo[i - memo[i - 1]]
+        memo.append(new_number)
+        output_string += f"{str(new_number)} "
 
-    output_string = ""
-    for n in nc_dict.keys():
-        output_string += f"{str(nc_dict[n])} "
     return output_string[:-1]
-
-def newman_conway_helper(n):
-    if n == 1:
-        return nc_dict[1]
-    elif n == 2:
-        return nc_dict[2]
-    elif n >= 3:
-        for i in range(3, n+1):
-            if i not in nc_dict:
-                nc_dict[i] = newman_conway_helper(nc_dict[i - 1]) + newman_conway_helper(i - nc_dict[i - 1])
-        
-        return newman_conway_helper(nc_dict[n - 1]) + newman_conway_helper(n - nc_dict[n - 1])
