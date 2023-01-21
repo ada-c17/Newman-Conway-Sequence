c3eb2279bdff6a0ff8cd4f6e05f49561a42f8035
Ivana Maldonado <ivana.a.maldonado@gmail.com>
2023-01-20 23:40:34 -0500

    Fiished newman conway sequence.

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -3,4 +3,17 @@ def newman_conway(num):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if num < 1:
+        raise ValueError
+    
+    if num == 1:
+        return "1"
+    
+    sequence = [0, 1, 1]
+
+    for i in range(3, num + 1):
+        sequence.append(sequence[sequence[i - 1]] + sequence[i - sequence[i - 1]])
+    sequence.pop(0)
+
+    str_sequence = [str(i) for i in sequence]
+    return " ".join(str_sequence)
