0e2b4ca1f4c32e5178eddc5f5b68edad03982272
camilla <pasadenastringtheory@gmail.com>
2023-01-20 23:42:52 -0800

    Implemented solution

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,6 +1,18 @@
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(N)
+        Space Complexity: O(N)
     """
-    pass
+    if num <= 0:
+        raise ValueError("Invalid input")
+    elif num == 1:
+        return "1"
+
+    sequence = [0] * (num + 1)
+    sequence[1] = 1
+    sequence[2] = 1
+
+    for i in range(3, num + 1):
+        sequence[i] = sequence[sequence[i - 1]] + sequence[i - sequence[i - 1]]
+
+    return " ".join([str(el) for el in sequence[1:]])
